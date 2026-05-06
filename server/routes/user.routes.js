d17c7d51223e5660dd6e6e34fcc6a90233940314
Abhay Prasad <167536919+Maurya-Abhay@users.noreply.github.com>
2026-05-06 19:51:39 +0530

    fix the all problem and add new fetures and chnage origin and fx

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getProfile, updateProfile, updateLocation, getLocation, applyRole } = require('../controllers/user.controller');
+const { getProfile, updateProfile, updateLocation, getLocation, applyRole, getRoleApplicationStatus } = require('../controllers/user.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 const { validateUserUpdate, validateLocationUpdate } = require('../middleware/validation.middleware');
 
